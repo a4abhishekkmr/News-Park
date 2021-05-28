3318e99fc535d22b538f42cd790ab6c423157808
a4abhishekkmr <a4abhishekkmr@gmail.com>
2021-05-28 12:23:22 +0530

    added List and toast till now(Recycler view)

--- app/src/main/java/com/example/newspark/MainActivity.kt
+++ app/src/main/java/com/example/newspark/MainActivity.kt
@@ -2,14 +2,33 @@ package com.example.newspark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NewsItemsClicked {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView.layoutManager=LinearLayoutManager(this)
+        val items=fetchData()
+        val adapter= NewsListAdapter(items,this)
+
+        //Now needed to link
+        recyclerView.adapter=adapter
+    }
+
+    private fun fetchData():ArrayList<String>{
+        val list=ArrayList<String>()
+        for(i in 0 until 100){//addded 100 items//which says total 100 list will be there
+            list.add("Item $i")
+        }
+        return list
+
+    }
+
+    override fun onItemclick(item: String) {
+        Toast.makeText(this,"clicked item is $item", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/newspark/NewsListAdapter.kt
+++ app/src/main/java/com/example/newspark/NewsListAdapter.kt
@@ -1,12 +1,47 @@
 package com.example.newspark
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NewsListAdapter: RecyclerView.Adapter<> {
+class NewsListAdapter(val items:ArrayList<String>,private val listener:NewsItemsClicked): RecyclerView.Adapter<NewsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        //Layout inflator added
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
+            //added item_news.xml as a view to be used
+        //which  is passed below.
+        //adding onclick
+        val viewHolder=NewsViewHolder(view)
+        view.setOnClickListener {
+            listener.onItemclick(items[viewHolder.adapterPosition])
+        }
+
+        return viewHolder
+        //Needed instance of NewsViewHolder
+    }
+
+    override fun onBindViewHolder(holder:NewsViewHolder, position: Int) {
+            //binds data
+        //position to bind
+        val currentItem= items[position]
+        holder.titleview.text=currentItem
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
 }
 
 class NewsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
     val titleview: TextView = itemView.findViewById(R.id.titlee)
+}
+
+//Added a callback
+//that tells that something is called here onclick listener
+//Added a interface for callback
+interface NewsItemsClicked{
+    fun onItemclick(item: String)
 }
